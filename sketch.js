02d374078c872123de62ad68bf0f741e6e0c0727
Ben Woods <woodsy867@gmail.com>
2019-08-06 11:34:53 +0800

    making it compatible with javascript

--- sketch.js
+++ sketch.js
@@ -1,23 +1,37 @@
+trial = 10000;
+dsize = 10;
+rolls = 2;
 
-function setup() {
-  createCanvas(400,400);
-  background(51);
-}
-p = 0;
-s = 0;
-points = []
-
-function draw() {
-  s ++;
-  pa = Math.random() * 2 - 1;
-  pb = Math.random() * 2 - 1;
-  if(Math.sqrt(pa*pa+pb*pb) < 1){
-    stroke('green');
-    p ++;
-  } else {
-    stroke('red');
+let sketch = function(p) {
+  p.setup = function(){
+    p.createCanvas(800,800);
+    p.background(51);
+    histogram(p,trial,dsize,rolls);
   }
-  point((pa+1)*200,(pb+1)*200);
-  //console.log((4*p)/s);
-  
+};
+new p5(sketch,'container');
+
+function process(){
+  var form = document.getElementById('theForm').elements;
+  trial = int(form[0].value);
+  dsize = int(form[1].value);
+  rolls = int(form[2].value);
+  histogram(sketch,trial,dsize,rolls);
+}
+
+function histogram(p,t,d,r){
+  p.background(51);
+  nums = [];
+  size = d * r;
+  for (var x = 0; x < size; x++){
+    nums.push(0);
+  }
+  for(var x = 0; x < t; x++){
+    var temp = 0;
+    for(var y = 0; y < r; y++){
+      temp += p.int(p.random(1,d+1));
+    }
+    nums[temp-1]++;
+  }
+  nums.forEach((x,y) => {p.rect(y*(p.width/size),p.height,p.width/size,-x*(p.height/t)*(d/2));})
 }
