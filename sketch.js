66608984ab40ff7cc4df13e0650a1f794de87baa
Ben Woods <woodsy867@gmail.com>
2019-08-07 09:42:26 +0800

    making the form look pretty

--- sketch.js
+++ sketch.js
@@ -1,37 +1,58 @@
 trial = 10000;
 dsize = 10;
 rolls = 2;
-
+dice = [4,6,8,10,12,20,100];
 let sketch = function(p) {
   p.setup = function(){
     p.createCanvas(800,800);
     p.background(51);
-    histogram(p,trial,dsize,rolls);
+    histogram(p,trial,dsize,rolls,document.getElementById('theForm').elements);
   }
 };
-new p5(sketch,'container');
+_p = new p5(sketch,'container');
 
 function process(){
   var form = document.getElementById('theForm').elements;
-  trial = int(form[0].value);
-  dsize = int(form[1].value);
-  rolls = int(form[2].value);
-  histogram(sketch,trial,dsize,rolls);
+  trial = parseInt(form[2].value);
+  dsize = dice[parseInt(form[0].value)];
+  rolls = parseInt(form[1].value);
+  for(var x=0;x < form.length;x++){form[x].disabled=true;}
+  setTimeout(function(){histogram(_p,trial,dsize,rolls,form)},5);
+  
 }
 
-function histogram(p,t,d,r){
+function histogram(p,t,d,r,f){
   p.background(51);
-  nums = [];
-  size = d * r;
-  for (var x = 0; x < size; x++){
-    nums.push(0);
-  }
+  size = ((d-1) * r)+1;
+  var nums = new Array(size).fill(0);
   for(var x = 0; x < t; x++){
     var temp = 0;
     for(var y = 0; y < r; y++){
       temp += p.int(p.random(1,d+1));
     }
-    nums[temp-1]++;
+    nums[temp-r]++;
   }
-  nums.forEach((x,y) => {p.rect(y*(p.width/size),p.height,p.width/size,-x*(p.height/t)*(d/2));})
+  if (size > 600){
+    p.stroke('white');
+  }else{
+    p.stroke('black');
+  }
+  var ma = Math.max.apply(null,nums);
+  nums.forEach((x,y) => {p.rect(y*(p.width/size),p.height,p.width/size,-(x/ma)*600);})
+  for(var x=0;x < f.length;x++){f[x].disabled=false;}
 }
+
+function diceUpdate(){
+  var di = dice[document.getElementById('dice').value];
+  document.getElementById('diceimg').src='diceImages/d'+di+'.png';
+  document.getElementById('diceLabel').innerHTML = '(d'+di+')';
+  process();
+}
+function rollUpdate(){
+  document.getElementById('rollsLabel').innerHTML = document.getElementById('roll').value;
+  process();
+}
+// function iterUpdate(){
+//   document.getElementById('iterLabel').innerHTML = document.getElementById('iter').value;
+//   process();
+// }
